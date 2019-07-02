ca6b8deed4b76c648e6074286a42b755457eb1d0
zouyun <9caonimama>
2019-07-02 20:07:34 +0800

    调试了bug，创建了函数，还没有写，今天上班比较忙

--- 数据结构/图/无向图.py
+++ 数据结构/图/无向图.py
@@ -1,3 +1,9 @@
+# author:
+
+
+
+
+
 class BaseGraph:
     """
     基本图的类，具有最简单的图的基本属性
@@ -77,11 +83,12 @@ class BaseGraph:
                             temp_list.remove(i)
                             self.list_dict[i] = temp_list
                     elif self.method == 'adjacency_matrix':
+                        # TODO 有bug  无法添加上
                         i_index = all_links.index(i)
-                        index_list = [self.nodes.index(j) for j in all_links[(i_index + 1):]]
+                        index_list = [j for j in all_links[(i_index + 1):]]   # 获取当前节点后面的 节点列表
                         for j in index_list:
-                            if j != i_index:    # 相等的不更新成1
-                                self.adjacency_matrix[j][i_index], self.adjacency_matrix[i_index][j] = 1, 1
+                            if j != i:    # 相等的不更新成1
+                                self.adjacency_matrix[self.nodes.index(j)][self.nodes.index(i)], self.adjacency_matrix[self.nodes.index(i)][self.nodes.index(j)] = 1, 1
 
         __add_links_inner(all_links=all_links)
         if links_list:
@@ -94,14 +101,20 @@ class BaseGraph:
         :type matrix: boolean 是否输出矩阵
         :return:
         """
+        print(''.center(40, '='))
+        print(f'图类型：{self.method}')
+        # TODO 图的大小
+        print(f'图大小：还没弄，哈哈')
         if matrix:
             if self.method == 'adjacency_matrix':
                 # print(self.adjacency_matrix)
+                print()
                 for i in self.adjacency_matrix:
                     # print(i)
                     for j in i:
                         print(j, end=' ')
-                    print()
+                    print('')
+                print('')
             else:
                 print('图的类型不是邻接矩阵类型( adjacency_matrix )，无法输出矩阵。')
         else:
@@ -110,12 +123,30 @@ class BaseGraph:
                     print(f'{i}: {k}')
             elif self.method == 'adjacency_matrix':
                 for i in range(len(self.nodes)):
-                    print(f'{self.nodes[i]}: ', end='')
+                    print(f'{self.nodes[i]}: [', end='')
                     for j in range(len(self.adjacency_matrix[i])):
                         if self.adjacency_matrix[i][j]:
                             print(f'{self.nodes[j]}, ', end='')
                     else:
-                        print()
+                        print(']')
+
+    def transform_method(self, to_method='adjacency_list'):
+        ...
+
+    def del_nodes(self, *args):
+        ...
+        def del_node(node):
+            ...
+
+    def del_links(self, *args):
+        # 参数可能是list fill with tuple
+        ...
+
+    def get_ways(self, a, b, time_id=False, type='all'):
+        # type min, max
+        ...
+
+
 
 
 
@@ -127,5 +158,6 @@ if __name__ == '__main__':
     n.add_node(5)
     n.add_node(7, neighbor_objs=[5, 6, 8, 10])
     n.add_links(all_links=[5, 3])
+    n.add_links(links_list=[[6, 8], [2, 10]])
     n.describe(matrix=True)
     n.describe()
