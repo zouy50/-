620bb14d64e566731220f99d892c99a8378c55d3
zouyun <9caonimama>
2019-11-16 10:50:25 +0800

    学生事件森林模型

---
+++ 其它/play_echarts/tree.js
@@ -0,0 +1,89 @@
+data = {
+    "name": "学生",
+    "children": [
+        {"name": "最近学习分析",
+         "children": [
+             {"name": "听讲专注",
+              "children": [{"name": "提高",
+                            "children": [
+                                {"name": "表扬孩子最近表现"},
+                                {"name": "分析课频, 如果低建议加课频"}
+                            ],
+                            },
+                           {"name": "降低",
+                            "children": [
+                                {"name": "提升专注度的方法"},
+                                {"name": "最近课程难度是否偏难"},
+                                {"name": "老师是否需要换"}
+                            ]}
+                           ]
+              },
+             {"name": "口语表达",
+              "children": [
+                  {"name": "提高",
+                   "children": [
+                       {"name": "表扬孩子口语表达提高"},
+                       {"name": "是否喜欢这个老师,推荐课"},
+                   ],
+                   },
+                  {"name": "降低",
+                   "children": [
+                       {"name": "年龄级别是否适合自然拼读",
+                        "children": [
+                            {"name": "适合",
+                             "children": [
+                                 {"name": "推荐自然拼读"}
+                             ]},
+                        ]},
+                       {"name": "上过自然拼读",
+                        "children": [
+                            {"name": ""},
+                        ]},
+                       {"name": "UA分数比较高"},
+                       {"name": "UA分数比较低"},
+                       {"name": "老师是否稳定喜不喜欢"}
+                   ]}
+              ]},
+             {"name": "上课活跃",
+              "children": [
+                  {"name": "提高",
+                   "children": [
+                       {"name": "表扬"},
+                   ]},
+                  {"name": "降低",
+                   "children": [
+                       {"name": "教师是否稳定"},
+                       {"name": "课程难度问题"},
+                   ]},
+              ]},
+             {"name": "UA分数",
+              "children": [
+                  {"name": "降低",
+                   "children": [
+                       {"name": ""},
+                   ]},
+                  {"name": "提高",
+                   "children": [
+                       {"name": ""},
+                   ]}
+              ]},
+             {"name": "作业完成度",
+              "children": [
+                  {"name": "降低",
+                   "children": [
+                       {"name": ""}
+                   ]},
+              ]},
+             {"name": "作业错题率",
+              "children": [
+                  {"name": "降低",
+                   "children": [
+
+                   ]},
+              ]},
+             {"name": ".其它.."},
+         ]},
+        {"name": "课消分析"},
+        {"name": ""},
+    ]
+}
